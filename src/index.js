4e7050d7d0d5d32d3081dcbc78fcf2abe4a48c38
TatsianaTarasova <70853063+TatsianaTarasova@users.noreply.github.com>
2020-10-22 18:56:53 +0200

    init : result task morse

--- src/index.js
+++ src/index.js
@@ -38,7 +38,17 @@ const MORSE_TABLE = {
 };
 
 function decode(expr) {
-    // write your solution here
+     const Arr = [];
+    const res = [];
+    for (let i = 0; i < expr.length; i += 10) {
+          Arr.push(expr.slice(i, i + 10).replace(/11/g, '-').replace(/10/g, '.').replace(/0/g, ''));
+    }
+    for (let i = 0; i < Arr.length; i++) {
+      if (Arr[i] in MORSE_TABLE) {
+          res.push(MORSE_TABLE[Arr[i]]);
+      } else res.push(' ');
+    }
+    return res.join('');
 }
 
 module.exports = {
